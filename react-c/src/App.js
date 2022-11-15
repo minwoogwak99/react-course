2b1d0ad54bdf9226eeb330501949c4c5c8e31bfa
Minwoo Gwak <mgwak2@illinois.edu>
2022-11-15 14:54:45 +0000

    Splitting componenets into Multiple componenets

--- react-c/src/App.js
+++ react-c/src/App.js
@@ -29,18 +29,23 @@ function App() {
       <div>This is also visible!</div>
       <ExpenseItem
         title={expenses[0].title}
-        date={expenses[0].date.toISOString()}
+        date={expenses[0].date}
         price={expenses[0].amount}
       />
       <ExpenseItem
         title={expenses[1].title}
-        date={expenses[1].date.toISOString()}
+        date={expenses[1].date}
         price={expenses[1].amount}
       />
       <ExpenseItem
         title={expenses[2].title}
-        date={expenses[2].date.toISOString()}
+        date={expenses[2].date}
         price={expenses[2].amount}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        date={expenses[3].date}
+        price={expenses[3].amount}
       />
     </div>
   );
