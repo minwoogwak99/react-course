182f1b58b85c5aa4385d42fae4c41f567ad3b7cf
Minwoo Gwak <mgwak2@illinois.edu>
2022-11-25 10:07:32 +0000

    two way binding

--- react-c/src/components/NewExpense/ExpenseForm.js
+++ react-c/src/components/NewExpense/ExpenseForm.js
@@ -51,6 +51,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -58,7 +61,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -66,6 +73,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHander}
           />
         </div>
@@ -75,6 +83,7 @@ const ExpenseForm = () => {
             type="date"
             min="2020-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
